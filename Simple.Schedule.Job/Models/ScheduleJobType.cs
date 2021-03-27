d9ead6572f2d2e77d35c2a5a563604612f180192
Juan Ramos <ptjuanramos@gmail.com>
2021-03-27 18:40:18 +0000

    Moving from old repo to new repo with Apache License 2.0

---
+++ Simple.Schedule.Job/Models/ScheduleJobType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Simple.Schedule.Job.Models
+{
+    public enum ScheduleJobType
+    {
+        Always,
+        Daily,
+        Hourly,
+        Weekly,
+        Monthly
+    }
+}
